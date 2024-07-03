4b6488cfd1b5178c789eca5b8b2dbe5717e7cd8a
Jay Rathod <rathodjay4397@gmail.com>
2024-07-04 00:40:06 +0530

    completed login and registration

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,19 +1,20 @@
 import { Link } from "react-router-dom";
 
-const LoginPage = () => {
+const RegisterPage = () => {
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
-        <h1 className="text-4xl text-center mb-4">Login</h1>
+        <h1 className="text-4xl text-center mb-4">Register</h1>
         <form className="max-w-md mx-auto ">
+          <input type="text" placeholder="Jay Rathod" />
           <input type="email" placeholder="your@email.com" />
           <input type="password" placeholder="password" />
-          <button className="primary">Login</button>
+          <button className="primary">Register</button>
           <div className="text-center py-1">
             <p className="text-sm text-gray-500">
-              {"Don't have an account? Sign up "}
-              <Link to={"/register"} className="underline text-primary">
-                here
+              {"Already Member? "}
+              <Link to={"/login"} className="underline text-primary">
+                Login
               </Link>
             </p>
           </div>
@@ -23,4 +24,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default RegisterPage;
