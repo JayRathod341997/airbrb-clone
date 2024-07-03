c7b495bb517d575ab1ed3da55b5fa6812509bce9
Jay Rathod <rathodjay4397@gmail.com>
2024-07-04 01:07:22 +0530

    added cors and axios

--- api/index.js
+++ api/index.js
@@ -1,1 +1,18 @@
-console.log("Hello");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+app.get("/test", (req, res) => {
+  res.send("Testing.....");
+});
+
+app.listen(4000, () => {
+  console.log("Server is running on port 4000");
+});
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-
+import axios from "axios";
 const RegisterPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const registerUser = (event) => {
+    event.preventDefault();
+    axios.get("http://localhost:4000/test");
+  };
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
         <h1 className="text-4xl text-center mb-4">Register</h1>
-        <form className="max-w-md mx-auto ">
+        <form className="max-w-md mx-auto" onSubmit={registerUser}>
           <input type="text" placeholder="Jay Rathod" />
           <input type="email" placeholder="your@email.com" />
           <input type="password" placeholder="password" />
