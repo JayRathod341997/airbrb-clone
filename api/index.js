84f4f6ecbd82cb4fa4f8c2a442d20aa1f465d9e0
Jay Rathod <rathodjay4397@gmail.com>
2024-07-06 00:05:05 +0530

    login module is added

--- api/index.js
+++ api/index.js
@@ -33,7 +33,23 @@ app.post("/register", async (req, res) => {
   }
 });
 
-//
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const isMatch = bcrypt.compareSync(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    res.json({ message: "Logged in successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,13 +1,38 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-
+import axios from "axios";
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const formSubmitHandler = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/login", { email, password });
+      alert("Login successfully");
+    } catch (e) {
+      console.error(e);
+      alert("Invalid credentials. Please try again");
+      // setEmail("");
+      // setPassword("");
+    }
+  };
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
         <h1 className="text-4xl text-center mb-4">Login</h1>
-        <form className="max-w-md mx-auto ">
-          <input type="email" placeholder="your@email.com" />
-          <input type="password" placeholder="password" />
+        <form className="max-w-md mx-auto " onSubmit={formSubmitHandler}>
+          <input
+            type="email"
+            placeholder="your@email.com"
+            value={email}
+            onChange={(ev) => setEmail(ev.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(ev) => setPassword(ev.target.value)}
+          />
           <button className="primary">Login</button>
           <div className="text-center py-1">
             <p className="text-sm text-gray-500">
